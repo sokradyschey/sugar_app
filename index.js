7227d61be9856c4ddd9aa09d52f3dc05105c1674
Sokrady Chey <sokradychey@gmail.com>
2022-10-02 16:55:05 -0400

    created glucose form

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
+
 let app = express();
+
 const port = 3000;
 
-app.use(express.static(path.join(__dirname,'views')))
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/images', express.static(__dirname + '/images'));
 
 app.get('/',(req,res)=>{
   res.sendFile('index.html')
